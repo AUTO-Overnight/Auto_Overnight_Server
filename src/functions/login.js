0ad73f8a7308021cf226e7d3bc17e4a3721db59c
이서윤 <idfn3535@gmail.com>
2022-04-22 23:04:43 +0900

    Refactor : require -> import로 변경, login function 분리

---
+++ src/functions/login.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const xmls = require("./xmls");
+const requestFunc = require("./requestFunc");
+
+export default async function loginfunction(axios, user) {
+
+  let yy;               //년도
+  let tmGbn;            //학기 구분 [1 : 1학기 / 2 : 2학기 / 5 : 여름학기 / 6 : 겨울학기]
+  let userNm;           //학생 이름
+  let persNo;           //학번
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+
+  let cookies;          //세션과 쿠키 저장 위한 변수
+  let base64encode;     //base64 encode
+
+  let outStayFrDt = [];
+  let outStayToDt = [];
+  let outStayStGbn = [];
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // 처음 로그인 하기
+  await axios.post("https://ksc.tukorea.ac.kr/sso/login_proc.jsp?returnurl=null", user)
+  .then()
+  .catch((e) => {
+    console.log(e);
+    requestFunc.makeErrorResponse("첫 로그인 실패", callback);
+  });
+
+  // 로그인 하기 위한 base64 encode
+  base64encode = Buffer.from(id, "utf8").toString('base64');
+
+  // 통정시 로그인, 쿠키 저장하기
+  await axios.get(`https://dream.tukorea.ac.kr/com/SsoCtr/initPageWork.do?loginGbn=sso&loginPersNo=${base64encode}`)
+  .then((res)=>{
+    cookies = res.config.headers.Cookie;
+  })
+  .catch((e) => {
+    requestFunc.makeErrorResponse("통합 정보 시스템 로그인 실패", callback)
+  });
+
+  // 학생 이름, 학번 찾기
+  await requestFunc.findUserName(xmls.findUserNmXML, axios)
+  .then((res) => {
+      let $ = cheerio.load(res.data, {
+          xmlMode: true
+      });
+      userNm = $('Col[id="userNm"]').text();
+      persNo = $('Col[id="persNo"]').text();
+      
+      if (userNm == "") {
+        requestFunc.makeErrorResponse("비밀번호가 맞지 않습니다.", callback);
+      }
+  })
+  .catch((e) => {
+    console.log(e);
+    requestFunc.makeErrorResponse("학생 이름 / 학번 찾기 실패", callback);
+  });
+
+  // 년도, 학기 찾기
+  await requestFunc.findYYtmgbn(xmls.findYYtmgbnXML, axios)
+  .then((res) => {
+      let $ = cheerio.load(res.data, {
+          xmlMode: true
+      });
+
+      yy = $('Col[id="yy"]').text(yy);
+      tmGbn = $('Col[id="tmGbn"]').text(tmGbn);
+  })
+  .catch((e) =>{
+    console.log(e);
+    requestFunc.makeErrorResponse("년도, 학기 찾기 실패", callback);
+  });
+
+  // 생활관 거주 학생 구분 번호 찾기 위한 xml 만들기 => 외박 신청 내역 조회 때 사용하는 xml과 같음
+  let findLiveStuNoXML = xmls.makeFindLiveStuNoXML(yy, tmGbn, persNo, userNm);
+
+  // 외박 신청 내역 조회하기
+  await requestFunc.findStayOutList(findLiveStuNoXML, axios)
+  .then((res) =>{ 
+    requestFunc.parseStayOutList(res, outStayFrDt, outStayToDt, outStayStGbn);
+  })
+  .catch((e) => {
+    console.log(e);
+    requestFunc.makeErrorResponse("외박 신청 내역 요청 실패", callback);
+  });
+
+  const body  = {
+    "cookies" : cookies,
+    "name" : userNm,
+    "yy" : yy,
+    "tmGbn" : tmGbn,
+    "outStayFrDt" : outStayFrDt,
+    "outStayToDt" : outStayToDt,
+    "outStayStGbn" : outStayStGbn
+  };
+
+  return body;
+}
--- src/handler.js
+++ src/handler.js
@@ -1,17 +1,20 @@
 'use strict';
 
-const xmls = require("./xmls");
-const requestFunc = require("./requestFunc");
+import xmls from "./xmls";
+import requestFunc from "./requestFunc";
 
-const axios = require('axios');
-const cheerio = require('cheerio');
-const axiosCookieJarSupport = require('axios-cookiejar-support').default;
-const tough = require('tough-cookie');
+import loginfunction from "./functions/login";
 
+import axios from "axios";
+import cheerio from "cheerio";
+import axiosCookieJarSupport from "axios-cookiejar-support";
+import tough from "tough-cookie";
+
+// 세션 유지 위한 쿠키 설정
 axiosCookieJarSupport(axios);
 
 axios.defaults.withCredentials = true;      // 쿠키 저장 허용
-axios.defaults.maxRedirects = 10;           // 리다이렉트 최댓값 -> 10은 되야 에러 안남             // 세션 유지 위한 쿠키 설정
+axios.defaults.maxRedirects = 10;           // 리다이렉트 최댓값 -> 10은 되야 에러 안남 
 axios.defaults.headers.post["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36";
 axios.defaults.headers.get["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36";
 
@@ -22,105 +25,14 @@ module.exports.login = async (event, context, callback) => {
   const cookieJar = new tough.CookieJar(); 
   axios.defaults.jar = cookieJar;
 
-  /////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  let yy;               //년도
-  let tmGbn;            //학기 구분 [1 : 1학기 / 2 : 2학기 / 5 : 여름학기 / 6 : 겨울학기]
-  let userNm;           //학생 이름
-  let persNo;           //학번
-
-  /////////////////////////////////////////////////////////////////////////////////////////////////
-
-  let cookies;          //세션과 쿠키 저장 위한 변수
-  let base64encode;     //base64 encode
-
-  let outStayFrDt = [];
-  let outStayToDt = [];
-  let outStayStGbn = [];
-
-  /////////////////////////////////////////////////////////////////////////////////////////////////
-
-  // 처음 로그인 하기
-  await axios.post("https://ksc.tukorea.ac.kr/sso/login_proc.jsp?returnurl=null", user)
-  .then()
-  .catch((e) => {
-    console.log(e);
-    requestFunc.makeErrorResponse("첫 로그인 실패", callback);
-  });
-
-  // 로그인 하기 위한 base64 encode
-  base64encode = Buffer.from(id, "utf8").toString('base64');
-
-  // 통정시 로그인, 쿠키 저장하기
-  await axios.get(`https://dream.tukorea.ac.kr/com/SsoCtr/initPageWork.do?loginGbn=sso&loginPersNo=${base64encode}`)
-  .then((res)=>{
-    cookies = res.config.headers.Cookie;
-  })
-  .catch((e) => {
-    requestFunc.makeErrorResponse("통합 정보 시스템 로그인 실패", callback)
-  });
-
-  // 학생 이름, 학번 찾기
-  await requestFunc.findUserName(xmls.findUserNmXML, axios)
-  .then((res) => {
-      let $ = cheerio.load(res.data, {
-          xmlMode: true
-      });
-      userNm = $('Col[id="userNm"]').text();
-      persNo = $('Col[id="persNo"]').text();
-      
-      if (userNm == "") {
-        requestFunc.makeErrorResponse("비밀번호가 맞지 않습니다.", callback);
-      }
-  })
-  .catch((e) => {
-    console.log(e);
-    requestFunc.makeErrorResponse("학생 이름 / 학번 찾기 실패", callback);
-  });
-
-  // 년도, 학기 찾기
-  await requestFunc.findYYtmgbn(xmls.findYYtmgbnXML, axios)
-  .then((res) => {
-      let $ = cheerio.load(res.data, {
-          xmlMode: true
-      });
-
-      yy = $('Col[id="yy"]').text(yy);
-      tmGbn = $('Col[id="tmGbn"]').text(tmGbn);
-  })
-  .catch((e) =>{
-    console.log(e);
-    requestFunc.makeErrorResponse("년도, 학기 찾기 실패", callback);
-  });
-
-  // 생활관 거주 학생 구분 번호 찾기 위한 xml 만들기 => 외박 신청 내역 조회 때 사용하는 xml과 같음
-  let findLiveStuNoXML = xmls.makeFindLiveStuNoXML(yy, tmGbn, persNo, userNm);
-
-  // 외박 신청 내역 조회하기
-  await requestFunc.findStayOutList(findLiveStuNoXML, axios)
-  .then((res) =>{ 
-    requestFunc.parseStayOutList(res, outStayFrDt, outStayToDt, outStayStGbn);
-  })
-  .catch((e) => {
-    console.log(e);
-    requestFunc.makeErrorResponse("외박 신청 내역 요청 실패", callback);
-  });
-
-  const body  = {
-    "cookies" : cookies,
-    "name" : userNm,
-    "yy" : yy,
-    "tmGbn" : tmGbn,
-    "outStayFrDt" : outStayFrDt,
-    "outStayToDt" : outStayToDt,
-    "outStayStGbn" : outStayStGbn
-  };
+  const body = loginfunction(axios, user);
 
   // 200 코드, 쿠키, 학생 이름, 외박 신청 내역 return
   return {
     statusCode: 200,
     body: JSON.stringify(body)
   };
+  
 };
 
 module.exports.sendStayOut = async (event, context, callback) => {
