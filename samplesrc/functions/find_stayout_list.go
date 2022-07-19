18d4fb4e3fc98d954dd1706cc0f434122f2af7bb
이서윤 <idfn3535@gmail.com>
2022-07-20 00:29:18 +0900

    Feat : 외박신청 내역 찾는 함수 완성

--- samplesrc/functions/find_stayout_list.go
+++ samplesrc/functions/find_stayout_list.go
@@ -1,7 +1,83 @@
 package functions
 
-import "github.com/aws/aws-lambda-go/events"
+import (
+	"auto_overnight_api/xmls"
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"net/http"
+	"net/http/cookiejar"
+	"sync"
+)
 
 func FindStayOutList(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{}, nil
+	var requestsModel FindRequestModel
+	err := json.Unmarshal([]byte(request.Body), &requestsModel)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	client := &http.Client{
+		Jar: jar,
+	}
+
+	findUserNmChan := make(chan xmls.Root)
+	findYYtmgbnChan := make(chan xmls.Root)
+
+	go xmls.RequestFindUserNm(client, findUserNmChan, requestsModel.Cookies)
+	go xmls.RequestFindYYtmgbn(client, findYYtmgbnChan, requestsModel.Cookies)
+
+	studentInfo := <-findUserNmChan
+	yytmGbnInfo := <-findYYtmgbnChan
+
+	stayOutList := xmls.RequestFindStayOutList(client, studentInfo, yytmGbnInfo, requestsModel.Cookies)
+
+	responseBody := make(map[string]interface{})
+
+	outStayFrDt := make([]string, len(stayOutList.Dataset[1].Rows.Row))
+	outStayToDt := make([]string, len(stayOutList.Dataset[1].Rows.Row))
+	outStayStGbn := make([]string, len(stayOutList.Dataset[1].Rows.Row))
+
+	var wg sync.WaitGroup
+	wg.Add(len(stayOutList.Dataset[1].Rows.Row))
+
+	go func() {
+		for i, v := range stayOutList.Dataset[1].Rows.Row[:len(stayOutList.Dataset[1].Rows.Row)/2] {
+			go func(i int, v xmls.Row) {
+				outStayFrDt[i] = v.Col[2].Data
+				outStayToDt[i] = v.Col[1].Data
+				outStayStGbn[i] = v.Col[0].Data
+				wg.Done()
+			}(i, v)
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i, v := range stayOutList.Dataset[1].Rows.Row[len(stayOutList.Dataset[1].Rows.Row)/2:] {
+			i += len(stayOutList.Dataset[1].Rows.Row) / 2
+			go func(i int, v xmls.Row) {
+				outStayFrDt[i] = v.Col[2].Data
+				outStayToDt[i] = v.Col[1].Data
+				outStayStGbn[i] = v.Col[0].Data
+				wg.Done()
+			}(i, v)
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+
+	responseBody["outStayFrDt"] = outStayFrDt
+	responseBody["outStayToDt"] = outStayToDt
+	responseBody["outStayStGbn"] = outStayStGbn
+
+	responseJson, _ := json.Marshal(responseBody)
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(responseJson),
+	}
+	return response, nil
 }
