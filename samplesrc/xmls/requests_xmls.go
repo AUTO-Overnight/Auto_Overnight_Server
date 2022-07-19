69accc64852b9b5896ae31f5be0f42fc80031e4d
이서윤 <idfn3535@gmail.com>
2022-07-19 23:47:04 +0900

    Feat : 외박 신청 찾는 함수 추가

--- samplesrc/xmls/requests_xmls.go
+++ samplesrc/xmls/requests_xmls.go
@@ -3,6 +3,7 @@ package xmls
 import (
 	"bytes"
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -67,4 +68,55 @@ func FindUserNm(client *http.Client, findUserNmChan chan Root) {
 	xml.Unmarshal(body, &studentInfo)
 
 	findUserNmChan <- studentInfo
+}
+
+func FindStayOut(client *http.Client, studentInfo, yytmGbnInfo Root) Root {
+	findLiveStuNoXML := []byte(fmt.Sprintf(`<?xmls version="1.0" encoding="UTF-8"?>
+    <Root xmlns="http://www.nexacroplatform.com/platform/dataset">
+        <Parameters>
+            <Parameter id="_ga">GA1.3.1065330987.1626699518</Parameter>
+            <Parameter id="requestTimeStr">1626877490927</Parameter>
+        </Parameters>
+        <Dataset id="DS_COND">
+            <ColumnInfo>
+                <Column id="yy" type="STRING" size="256"  />
+                <Column id="tmGbn" type="STRING" size="256"  />
+                <Column id="schregNo" type="STRING" size="256"  />
+                <Column id="stdKorNm" type="STRING" size="256"  />
+                <Column id="outStayStGbn" type="STRING" size="256"  />
+            </ColumnInfo>
+            <Rows>
+                <Row type="update">
+                    <Col id="yy">%s</Col>
+                    <Col id="tmGbn">%s</Col>
+                    <Col id="schregNo">%s</Col>
+                    <Col id="stdKorNm">%s</Col>
+                    <OrgRow>
+                    </OrgRow>
+                </Row>
+            </Rows>
+        </Dataset>
+    </Root>`,
+		yytmGbnInfo.Dataset[0].Rows.Row[0].Col[0].Data,
+		yytmGbnInfo.Dataset[0].Rows.Row[0].Col[1].Data,
+		studentInfo.Dataset[0].Rows.Row[0].Col[1].Data,
+		studentInfo.Dataset[0].Rows.Row[0].Col[0].Data))
+
+	req, err := http.NewRequest("POST", "https://dream.tukorea.ac.kr/aff/dorm/DormCtr/findStayAplyList.do?menuId=MPB0022&pgmId=PPB0021", bytes.NewBuffer(findLiveStuNoXML))
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	body, _ := ioutil.ReadAll(res.Body)
+	var temp Root
+	err = xml.Unmarshal(body, &temp)
+	if err != nil {
+		panic(err)
+	}
+
+	return temp
 }
