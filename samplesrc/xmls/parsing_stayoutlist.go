5cd2fa402bdf087dcfb01fc4131d1f93ca6f7be6
이서윤 <idfn3535@gmail.com>
2022-07-20 17:13:29 +0900

    Feat : 주석 추가

--- samplesrc/xmls/parsing_stayoutlist.go
+++ samplesrc/xmls/parsing_stayoutlist.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 )
 
+// ParsingStayoutList 외박 신청 내역 파싱하는 함수
 func ParsingStayoutList(stayOutList Root, outStayFrDtChan, outStayToDtChan, outStayStGbnChan chan []string) {
 
 	var wg sync.WaitGroup
@@ -15,6 +16,7 @@ func ParsingStayoutList(stayOutList Root, outStayFrDtChan, outStayToDtChan, outS
 	outStayToDt := make([]string, len(stayOutList.Dataset[1].Rows.Row))
 	outStayStGbn := make([]string, len(stayOutList.Dataset[1].Rows.Row))
 
+	// 파싱 시작
 	for i, v := range stayOutList.Dataset[1].Rows.Row {
 		go func(i int, v Row) {
 			outStayFrDt[i] = v.Col[2].Data
@@ -31,9 +33,12 @@ func ParsingStayoutList(stayOutList Root, outStayFrDtChan, outStayToDtChan, outS
 	outStayStGbnChan <- outStayStGbn
 }
 
+// ParsingCookies 쿠키 파싱하는 함수
 func ParsingCookies(req *http.Request, cookiesChan chan map[string]string) {
+	// 쿠키 파싱 위한 슬라이스 생성
 	cookies := make(map[string]string)
 
+	// 파싱 시작
 	for _, info := range req.Cookies() {
 		cookies[info.Name] = info.Value
 	}
