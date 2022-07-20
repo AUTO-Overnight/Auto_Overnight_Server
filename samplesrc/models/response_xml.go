37c3c188736c5d02307856638242288ce4875c8e
이서윤 <idfn3535@gmail.com>
2022-07-20 21:30:40 +0900

    Feat : 원본 xml 추가

--- samplesrc/models/response_xml.go
+++ samplesrc/models/response_xml.go
@@ -2,6 +2,27 @@ package models
 
 import "encoding/xml"
 
+/*
+<?xml version="1.0" encoding="UTF-8"?>
+<Root xmlns="http://www.nexacro.com/platform/dataset" ver="5000">
+	<Parameters>
+		<Parameter id="ErrorCode" type="int">0</Parameter>
+	</Parameters>
+	<Dataset id="DS_GLIO">
+		<ColumnInfo>
+			<Column id="userNm" type="string" size="32"/>
+			<Column id="persNo" type="string" size="32"/>
+		</ColumnInfo>
+		<Rows>
+			<Row>
+				<Col id="userNm">이름</Col>
+				<Col id="persNo">학번</Col>
+			</Row>
+		</Rows>
+	</Dataset>
+</Root>
+*/
+
 type Root struct {
 	XMLName    xml.Name   `xml:"Root"`
 	Parameters Parameters `xml:"Parameters"`
