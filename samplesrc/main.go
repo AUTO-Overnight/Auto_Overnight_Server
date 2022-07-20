a47df371a15564d495c109eaca029b36cf612e09
이서윤 <idfn3535@gmail.com>
2022-07-20 15:31:40 +0900

    Feat : 주석 추가

--- samplesrc/main.go
+++ samplesrc/main.go
@@ -6,22 +6,29 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// HandleRequest Path에 맞는 함수를 실행함
 func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var response events.APIGatewayProxyResponse
 	var err error
 
 	switch request.Path {
+	// 로그인
 	case "/login":
 		response, err = functions.Login(request)
+	// 외박 신청 보내기
 	case "/sendstayout":
 		response, err = functions.SendStayOut(request)
+	// 외박 신청 내역 조회
 	case "/findstayoutlist":
 		response, err = functions.FindStayOutList(request)
+	// 상벌점 조회
 	case "/findpointlist":
 		response, err = functions.FindPointList(request)
+	// 잘못된 Path
 	default:
 		response, err = events.APIGatewayProxyResponse{StatusCode: 404}, nil
 	}
+
 	return response, err
 }
 
