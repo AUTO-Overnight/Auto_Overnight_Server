cead293dd7db7f3604edba56b1dd449ffecf58b5
이서윤 <idfn3535@gmail.com>
2022-07-20 15:31:31 +0900

    Feat : 에러 처리 추가, 주석 추가, 파싱함수 고루틴으로 변경

--- samplesrc/functions/login.go
+++ samplesrc/functions/login.go
@@ -1,6 +1,7 @@
 package functions
 
 import (
+	"auto_overnight_api/error_response"
 	"auto_overnight_api/xmls"
 	"bytes"
 	"encoding/json"
@@ -8,53 +9,65 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
-	"sync"
 )
 
+// Login Id와 Password를 Json으로 입력 받아 로그인하고 이름, 년도, 학기, 쿠키, 외박 신청 내역을 return
 func Login(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var requestsModel LoginRequestModel
 	err := json.Unmarshal([]byte(request.Body), &requestsModel)
+
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		return error_response.MakeErrorResponse(error_response.ParsingJsonBodyError, 500)
+	}
+	if requestsModel.Id == "" || requestsModel.PassWord == "" {
+		return error_response.MakeErrorResponse(error_response.EmptyIdOrPasswordError, 400)
 	}
 
+	// x-www-form-urlencoded 방식으로 로그인 하기 위해 form 생성
 	loginInfo := url.Values{
 		"internalId": {requestsModel.Id},
 		"internalPw": {requestsModel.PassWord},
 		"gubun":      {"inter"},
 	}
 
+	// cookie jar 생성
 	jar, err := cookiejar.New(nil)
 	if err != nil {
-		panic(err)
+		return error_response.MakeErrorResponse(error_response.MakeCookieJarError, 500)
 	}
 
+	// 로그인 http request 생성
 	req, err := http.NewRequest("POST", "https://ksc.tukorea.ac.kr/sso/login_proc.jsp?returnurl=null", bytes.NewBufferString(loginInfo.Encode()))
 	if err != nil {
-		panic(err)
+		return error_response.MakeErrorResponse(error_response.MakeHttpRequestError, 500)
 	}
 
+	// Content-Type 헤더 설정, client에 cookie jar 설정
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	client := &http.Client{
 		Jar: jar,
 	}
+	// 로그인 시도
 	res, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return error_response.MakeErrorResponse(error_response.SendHttpRequestError, 500)
 	}
 
 	defer res.Body.Close()
 
+	// 통합 정보 시스템 세션 얻기 시도
 	req, err = http.NewRequest("GET", "https://dream.tukorea.ac.kr/com/SsoCtr/initPageWork.do?loginGbn=sso&loginPersNo=", nil)
 
 	if err != nil {
-		panic(err)
+		return error_response.MakeErrorResponse(error_response.MakeHttpRequestError, 500)
 	}
 
 	res, err = client.Do(req)
 	if err != nil {
-		panic(err)
+		return error_response.MakeErrorResponse(error_response.SendHttpRequestError, 500)
 	}
+
+	// 학생 이름, 학번, 년도, 학기 찾기 위한 채널 생성
 	findUserNmChan := make(chan xmls.Root)
 	findYYtmgbnChan := make(chan xmls.Root)
 
@@ -64,7 +77,12 @@ func Login(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespons
 	studentInfo := <-findUserNmChan
 	yytmGbnInfo := <-findYYtmgbnChan
 
-	stayOutList, req := xmls.RequestFindStayOutList(
+	if studentInfo.Parameters.Parameter == "-600" {
+		return error_response.MakeErrorResponse(error_response.WrongIdOrPasswordError, 400)
+	}
+
+	// 외박 신청 내역 조회
+	stayOutList, req, err := xmls.RequestFindStayOutList(
 		client,
 		yytmGbnInfo.Dataset[0].Rows.Row[0].Col[0].Data,
 		yytmGbnInfo.Dataset[0].Rows.Row[0].Col[1].Data,
@@ -72,58 +90,38 @@ func Login(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespons
 		studentInfo.Dataset[0].Rows.Row[0].Col[0].Data,
 		nil)
 
+	if err != nil {
+		return error_response.MakeErrorResponse(err, 500)
+	}
+
+	// 응답 위한 json body 만들기
 	responseBody := make(map[string]interface{})
+
+	// 이름, 년도, 학기 저장
 	responseBody["name"] = studentInfo.Dataset[0].Rows.Row[0].Col[0].Data
 	responseBody["yy"] = yytmGbnInfo.Dataset[0].Rows.Row[0].Col[0].Data
 	responseBody["tmGbn"] = yytmGbnInfo.Dataset[0].Rows.Row[0].Col[1].Data
 
-	cookies := make(map[string]string)
+	// 쿠키, 외박 신청 내역 파싱 내역 전달받기 위한 채널 생성
+	cookiesChan := make(chan map[string]string)
+	outStayFrDtChan := make(chan []string)
+	outStayToDtChan := make(chan []string)
+	outStayStGbnChan := make(chan []string)
 
-	outStayFrDt := make([]string, len(stayOutList.Dataset[1].Rows.Row))
-	outStayToDt := make([]string, len(stayOutList.Dataset[1].Rows.Row))
-	outStayStGbn := make([]string, len(stayOutList.Dataset[1].Rows.Row))
+	// 파싱 시작
+	go xmls.ParsingStayoutList(stayOutList)
+	go xmls.ParsingCookies(req, cookiesChan)
 
-	var wg sync.WaitGroup
-	wg.Add(len(stayOutList.Dataset[1].Rows.Row) + 3)
+	responseBody["cookies"] = <-cookiesChan
+	responseBody["outStayFrDt"] = <-outStayFrDtChan
+	responseBody["outStayToDt"] = <-outStayToDtChan
+	responseBody["outStayStGbn"] = <-outStayStGbnChan
 
-	go func() {
-		for i, v := range stayOutList.Dataset[1].Rows.Row[:len(stayOutList.Dataset[1].Rows.Row)/2] {
-			go func(i int, v xmls.Row) {
-				outStayFrDt[i] = v.Col[2].Data
-				outStayToDt[i] = v.Col[1].Data
-				outStayStGbn[i] = v.Col[0].Data
-				wg.Done()
-			}(i, v)
-		}
-		wg.Done()
-	}()
-	go func() {
-		for i, v := range stayOutList.Dataset[1].Rows.Row[len(stayOutList.Dataset[1].Rows.Row)/2:] {
-			i += len(stayOutList.Dataset[1].Rows.Row) / 2
-			go func(i int, v xmls.Row) {
-				outStayFrDt[i] = v.Col[2].Data
-				outStayToDt[i] = v.Col[1].Data
-				outStayStGbn[i] = v.Col[0].Data
-				wg.Done()
-			}(i, v)
-		}
-		wg.Done()
-	}()
-
-	go func() {
-		for _, info := range req.Cookies() {
-			cookies[info.Name] = info.Value
-		}
-		wg.Done()
-	}()
-	wg.Wait()
-
-	responseBody["cookies"] = cookies
-	responseBody["outStayFrDt"] = outStayFrDt
-	responseBody["outStayToDt"] = outStayToDt
-	responseBody["outStayStGbn"] = outStayStGbn
-
-	responseJson, _ := json.Marshal(responseBody)
+	// 응답 json 만들기
+	responseJson, err := json.Marshal(responseBody)
+	if err != nil {
+		return error_response.MakeErrorResponse(error_response.MakeJsonBodyError, 500)
+	}
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       string(responseJson),
