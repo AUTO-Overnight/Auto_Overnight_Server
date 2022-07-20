b8c3b1ae7fa757e084e939c6b7c49549bb1976f9
이서윤 <idfn3535@gmail.com>
2022-07-20 16:20:00 +0900

    Feat : 주석 추가

--- samplesrc/functions/request_model.go
+++ samplesrc/functions/request_model.go
@@ -1,10 +1,12 @@
 package functions
 
+// LoginRequestModel 로그인에 필요한 json
 type LoginRequestModel struct {
 	Id       string `json:"id"`
 	PassWord string `json:"password"`
 }
 
+// FindRequestModel 상벌점 찾기, 외박 신청 내역 조회에 필요한 json
 type FindRequestModel struct {
 	Year    string            `json:"yy"`
 	TmGbn   string            `json:"tmGbn"`
@@ -12,6 +14,7 @@ type FindRequestModel struct {
 	Cookies map[string]string `json:"cookies"`
 }
 
+// SendRequestModel 외박 신청에 필요한 json
 type SendRequestModel struct {
 	DateList      []string          `json:"date_list"`
 	IsWeekend     []int             `json:"is_weekend"`
