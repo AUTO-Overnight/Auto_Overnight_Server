2c5b760ed50013e314e85e894c3b7d959324f040
이서윤 <idfn3535@gmail.com>
2022-07-20 21:29:05 +0900

    Revert "Feat : 테스팅 코드 추가"
    This reverts commit 29e2226760520de58d3a5558af9eba1f3d5e88dc.

--- samplesrc/main_test.go
+++ samplesrc/main_test.go
@@ -1,100 +1,34 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"testing"
 )
 
-// TestHandleRequestLogin login 테스트
-func TestHandleRequestLogin(t *testing.T) {
-
-	response, err := HandleRequest(events.APIGatewayProxyRequest{
-		Path: "/login",
-		Body: `
-		{
-			"id" : "2018150030",
-			"password" : "toddlf0829_"
-		}`,
-	})
-
-	if response.StatusCode != 200 {
-		t.Log(response, err)
-		t.FailNow()
-	}
-	t.Log(response)
-}
-
-// TestHandleRequestFindStayOutList 외박 신청 내역 조회 테스트
-func TestHandleRequestFindStayOutList(t *testing.T) {
-
-	response, err := HandleRequest(events.APIGatewayProxyRequest{
-		Path: "/findstayoutlist",
-		Body: `
-		{
-			"yy" : "2022",
-			"tmGbn" : "5",
-			"userNm" : "이서윤",
-			"cookies": {
-				"JSVSESSIONID": "MjgadzJs5544V5yIo5NJtz71xSUC1ZM8Qf04RFLgt91ngXXs73JwerCo4MO0Gqt8.amV1c19kb21haW4vanN2XzI=",
-				"_SSO_Global_Logout_url": "get%5Ehttps%3A%2F%2Fportal.tukorea.ac.kr%3A443%2Fsso%2Flogout.jsp%24get%5Ehttps%3A%2F%2Fdream.tukorea.ac.kr%3A443%2Fcom%2FSsoCtr%2Fj_logout.do%24",
-				"kalogin": "NZ4uqEuKiTiR/A==$server"
-			}
-		}`,
-	})
-
-	if response.StatusCode != 200 {
-		t.Log(response, err)
-		t.FailNow()
-	}
-	t.Log(response)
-}
-
-// TestHandleRequestSendStayOut 외박 신청 테스트
-func TestHandleRequestSendStayOut(t *testing.T) {
-
+func TestHandleRequest(t *testing.T) {
 	response, err := HandleRequest(events.APIGatewayProxyRequest{
 		Path: "/sendstayout",
 		Body: `
-		{
-			"date_list" : ["20220721"],
-			"is_weekend" : [0],
-			"outStayAplyDt" : "20220720",
-			"cookies": {
-				"JSVSESSIONID": "MjgadzJs5544V5yIo5NJtz71xSUC1ZM8Qf04RFLgt91ngXXs73JwerCo4MO0Gqt8.amV1c19kb21haW4vanN2XzI=",
-				"_SSO_Global_Logout_url": "get%5Ehttps%3A%2F%2Fportal.tukorea.ac.kr%3A443%2Fsso%2Flogout.jsp%24get%5Ehttps%3A%2F%2Fdream.tukorea.ac.kr%3A443%2Fcom%2FSsoCtr%2Fj_logout.do%24",
-				"kalogin": "NZ4uqEuKiTiR/A==$server"
-			}
-		}`,
+			{
+    "date_list" : [20220722],
+    "is_weekend" : [0],
+    "outStayAplyDt" : "20220720",
+    "cookies": {
+        "JSVSESSIONID": "120jAshn5ZnpT52NrjLMSqbuN7CLgkpHQkHrhpS1VMtHEEaC7sOsGegjyaxTr1eT.amV1c19kb21haW4vanN2XzI=",
+        "_SSO_Global_Logout_url": "get%5Ehttps%3A%2F%2Fportal.tukorea.ac.kr%3A443%2Fsso%2Flogout.jsp%24get%5Ehttps%3A%2F%2Fdream.tukorea.ac.kr%3A443%2Fcom%2FSsoCtr%2Fj_logout.do%24",
+        "kalogin": "cJ8q8rvw2TIapw==$server"
+    }
+}`,
 	})
 
-	if response.StatusCode != 200 {
-		t.Log(response, err)
-		t.FailNow()
+	if err != nil {
+		t.Log(err)
+		t.Log(response)
+		t.Failed()
 	}
-	t.Log(response)
-}
+	val, err := json.MarshalIndent(response.Body, "", "    ")
+	fmt.Println(string(val))
 
-// TestHandleRequestFindPointList 상벌점 조회 테스트
-func TestHandleRequestFindPointList(t *testing.T) {
-
-	response, err := HandleRequest(events.APIGatewayProxyRequest{
-		Path: "/findpointlist",
-		Body: `
-		{
-			"yy" : "2021",
-			"tmGbn" : "2",
-			"userNm" : "이서윤",
-			"cookies": {
-				"JSVSESSIONID": "MjgadzJs5544V5yIo5NJtz71xSUC1ZM8Qf04RFLgt91ngXXs73JwerCo4MO0Gqt8.amV1c19kb21haW4vanN2XzI=",
-				"_SSO_Global_Logout_url": "get%5Ehttps%3A%2F%2Fportal.tukorea.ac.kr%3A443%2Fsso%2Flogout.jsp%24get%5Ehttps%3A%2F%2Fdream.tukorea.ac.kr%3A443%2Fcom%2FSsoCtr%2Fj_logout.do%24",
-				"kalogin": "NZ4uqEuKiTiR/A==$server"
-			}
-		}`,
-	})
-
-	if response.StatusCode != 200 {
-		t.Error(response, err)
-		t.FailNow()
-	}
-	t.Log(response)
 }
