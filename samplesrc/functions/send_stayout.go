31270ea9e384956772171dd5220b26e180d23539
이서윤 <idfn3535@gmail.com>
2022-07-20 14:17:24 +0900

    Fix : 외박 신청 내역 리턴하는 내용 추가

--- samplesrc/functions/send_stayout.go
+++ samplesrc/functions/send_stayout.go
@@ -6,15 +6,16 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"net/http"
 	"net/http/cookiejar"
+	"sync"
 )
 
 func SendStayOut(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var requestsModel SendRequestModel
 	err := json.Unmarshal([]byte(request.Body), &requestsModel)
+
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
 	}
-
 	jar, err := cookiejar.New(nil)
 	if err != nil {
 		panic(err)
@@ -44,5 +45,58 @@ func SendStayOut(request events.APIGatewayProxyRequest) (events.APIGatewayProxyR
 	if err != nil {
 		panic(err)
 	}
-	return events.APIGatewayProxyResponse{StatusCode: 201}, nil
+
+	stayOutList, _ := xmls.RequestFindStayOutList(
+		client,
+		yytmGbnInfo.Dataset[0].Rows.Row[0].Col[0].Data,
+		yytmGbnInfo.Dataset[0].Rows.Row[0].Col[1].Data,
+		studentInfo.Dataset[0].Rows.Row[0].Col[1].Data,
+		studentInfo.Dataset[0].Rows.Row[0].Col[0].Data,
+		requestsModel.Cookies)
+
+	responseBody := make(map[string]interface{})
+
+	outStayFrDt := make([]string, len(stayOutList.Dataset[1].Rows.Row))
+	outStayToDt := make([]string, len(stayOutList.Dataset[1].Rows.Row))
+	outStayStGbn := make([]string, len(stayOutList.Dataset[1].Rows.Row))
+
+	var wg sync.WaitGroup
+	wg.Add(len(stayOutList.Dataset[1].Rows.Row) + 2)
+
+	go func() {
+		for i, v := range stayOutList.Dataset[1].Rows.Row[:len(stayOutList.Dataset[1].Rows.Row)/2] {
+			go func(i int, v xmls.Row) {
+				outStayFrDt[i] = v.Col[2].Data
+				outStayToDt[i] = v.Col[1].Data
+				outStayStGbn[i] = v.Col[0].Data
+				wg.Done()
+			}(i, v)
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i, v := range stayOutList.Dataset[1].Rows.Row[len(stayOutList.Dataset[1].Rows.Row)/2:] {
+			i += len(stayOutList.Dataset[1].Rows.Row) / 2
+			go func(i int, v xmls.Row) {
+				outStayFrDt[i] = v.Col[2].Data
+				outStayToDt[i] = v.Col[1].Data
+				outStayStGbn[i] = v.Col[0].Data
+				wg.Done()
+			}(i, v)
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+
+	responseBody["outStayFrDt"] = outStayFrDt
+	responseBody["outStayToDt"] = outStayToDt
+	responseBody["outStayStGbn"] = outStayStGbn
+
+	responseJson, _ := json.Marshal(responseBody)
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(responseJson),
+	}
+
+	return response, nil
 }
