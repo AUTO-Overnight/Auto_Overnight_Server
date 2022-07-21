ce87751c3d5699ec251ad5ea43f9c864af6c4072
이서윤 <idfn3535@gmail.com>
2022-07-21 18:26:32 +0900

    Feat : 쿠키 구조체 삭제, 주석 추가

--- model/parse_response.go
+++ model/parse_response.go
@@ -1,15 +1,13 @@
 package model
 
-type Cookies struct {
-	Data map[string]string
-}
-
+// StayOutList 외박 신청 내역 파싱 위한 구조체
 type StayOutList struct {
 	OutStayFrDt  []string
 	OutStayToDt  []string
 	OutStayStGbn []string
 }
 
+// PointList 상벌점 내역 파싱 위한 구조체
 type PointList struct {
 	CmpScr        []string
 	LifSstArdGbn  []string
