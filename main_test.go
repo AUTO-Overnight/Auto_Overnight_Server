d51be28547191ff361fa265087631866e5284a01
이서윤 <idfn3535@gmail.com>
2022-09-06 14:21:53 +0900

    Refactor : 쿠키, id, pw 전역 변수로 변경

--- main_test.go
+++ main_test.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"auto_overnight_api/route"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
+	"net/url"
 	"testing"
 )
 
+var id = "2018150030"
+var pw = ""
+var cookies = ""
+
 // TestHandleRequestLogin login 테스트
 func TestHandleRequestLogin(t *testing.T) {
+	url.QueryEscape(pw)
 
 	response, err := route.Login(events.APIGatewayProxyRequest{
 		Path: "/login",
-		Body: `
+		Body: fmt.Sprintf(`
 		{
-			"id" : "",
-			"password" : ""
-		}`,
+			"id" : "%s",
+			"password" : "%s"
+		}`, id, pw),
 	})
 
 	if response.StatusCode != 200 {
@@ -30,13 +37,13 @@ func TestHandleRequestFindStayOutList(t *testing.T) {
 
 	response, err := HandleRequest(events.APIGatewayProxyRequest{
 		Path: "/findstayoutlist",
-		Body: `
+		Body: fmt.Sprintf(`
 		{
 			"yy" : "2022",
-			"tmGbn" : "5",
+			"tmGbn" : "2",
 			"userNm" : "이서윤",
-			"cookies": "aralZ3uJuRk1mOpKsiFGx4iD12Yyzbv09AKa1U4Z77ciz81a75ITcP0rESgsw4F6.amV1c19kb21haW4vanN2XzE="
-		}`,
+			"cookies": "%s"
+		}`, cookies),
 	})
 
 	if response.StatusCode != 200 {
@@ -51,13 +58,13 @@ func TestHandleRequestSendStayOut(t *testing.T) {
 
 	response, err := HandleRequest(events.APIGatewayProxyRequest{
 		Path: "/sendstayout",
-		Body: `
+		Body: fmt.Sprintf(`
 		{
-			"date_list" : ["20220902"],
+			"date_list" : ["20220801"],
 			"is_weekend" : [0],
-			"outStayAplyDt" : "20220721",
-			"cookies": "aralZ3uJuRk1mOpKsiFGx4iD12Yyzbv09AKa1U4Z77ciz81a75ITcP0rESgsw4F6.amV1c19kb21haW4vanN2XzE="
-		}`,
+			"outStayAplyDt" : "20220730",
+			"cookies": "%s"
+		}`, cookies),
 	})
 
 	if response.StatusCode != 200 {
@@ -72,13 +79,13 @@ func TestHandleRequestFindPointList(t *testing.T) {
 
 	response, err := HandleRequest(events.APIGatewayProxyRequest{
 		Path: "/findpointlist",
-		Body: `
+		Body: fmt.Sprintf(`
 		{
 			"yy" : "2022",
-			"tmGbn" : "1",
+			"tmGbn" : "2",
 			"userNm" : "이서윤",
-			"cookies": "aralZ3uJuRk1mOpKsiFGx4iD12Yyzbv09AKa1U4Z77ciz81a75ITcP0rESgsw4F6.amV1c19kb21haW4vanN2XzE="
-		}`,
+			"cookies": "%s"
+		}`, cookies),
 	})
 
 	if response.StatusCode != 200 {
