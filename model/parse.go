e033cb3a28474c57a48efd473f0aa22d85a8307b
이서윤 <idfn3535@gmail.com>
2022-12-22 00:39:16 +0900

    Refactor : 주석 수정

--- model/parse.go
+++ model/parse.go
@@ -1,13 +1,13 @@
 package model
 
-// StayOutList 외박 신청 내역 파싱 위한 구조체
+// StayOutList 외박 신청 내역 파싱하여 저장할 구조체
 type StayOutList struct {
 	OutStayFrDt  []string
 	OutStayToDt  []string
 	OutStayStGbn []string
 }
 
-// PointList 상벌점 내역 파싱 위한 구조체
+// PointList 상벌점 내역 파싱하여 저장할 구조체
 type PointList struct {
 	CmpScr        []string
 	LifSstArdGbn  []string
@@ -15,7 +15,7 @@ type PointList struct {
 	LifSstArdCtnt []string
 }
 
-// RequestInfo request 보내기 위한 년, 학기, 학생 정보 구조체
+// RequestInfo request 보내기 위한 년, 학기, 학번, 학생 이름 저장할 구조체
 type RequestInfo struct {
 	YY       string
 	TmGbn    string
