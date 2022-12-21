bd0cd0979b2ebcc3bd068b038c591506c9d5d5d3
이서윤 <idfn3535@gmail.com>
2022-12-22 00:17:02 +0900

    Feat : url config package 생성

---
+++ config/config.go
@@ -0,0 +1,11 @@
+package config
+
+var (
+	SchoolUrl       = "https://dream.tukorea.ac.kr/"
+	YearSemesterUrl = SchoolUrl + "aff/dorm/DormCtr/findYyTmGbnList.do?menuId=MPB0022&pgmId=PPB0021"
+	NameIdUrl       = SchoolUrl + "com/SsoCtr/findMyGLIOList.do?menuId=MPB0022&pgmId=PPB0021"
+	ApplyListUrl    = SchoolUrl + "aff/dorm/DormCtr/findStayAplyList.do?menuId=MPB0022&pgmId=PPB0021"
+	RewardListUrl   = SchoolUrl + "aff/dorm/DormCtr/findFindArdListList.do?menuId=MPB0024&pgmId=PPB0023"
+	DormStuIdUrl    = SchoolUrl + "aff/dorm/DormCtr/findMdstrmLeaveAplyList.do?menuId=MPB0022&pgmId=PPB0021"
+	SendApplyUrl    = SchoolUrl + "aff/dorm/DormCtr/saveOutAplyList.do?menuId=MPB0022&pgmId=PPB0021"
+)
