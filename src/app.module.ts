5fb5d4c77b9d83cf982dd627fc07da0777b32a83
kjeongh <wellsy001012@gmail.com>
2024-01-03 02:27:33 +0900

    feat: Global exception handler 설정 및 CustomException 정의

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { AuthController } from './controller/auth.controller';
 import { PointController } from './controller/point.controller';
 import { StayOutController } from './controller/stay-out.controller';
 import { LoggingModule } from './infra/logging/logging.module';
+import { GlobalExceptionFilter } from './global/filter/exception.filter';
 
 @Module({
   imports: [LoggingModule],
   controllers: [AuthController, PointController, StayOutController],
-  providers: [],
+  providers: [
+    {
+      provide: 'GLOBAL_EXCEPTION_FILTER',
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CustomLogger } from './infra/logging/custom-logger';
-import { schoolUrl } from './config';
+import dotenv = require('dotenv');
+import * as path from 'path';
+
+// env설정
+dotenv.config();
+const environment = process.env.NODE_ENV;
+const envPath =
+  environment === 'local'
+    ? '../.env-local'
+    : environment === 'dev'
+      ? '../.env-dev'
+      : '../.env-prod';
+dotenv.config({ path: path.join(__dirname, envPath) });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +21,7 @@ async function bootstrap() {
   });
   app.enableCors();
   app.useLogger(app.get(CustomLogger));
-  await app.listen(3000);
+  app.useGlobalFilters(app.get('GLOBAL_EXCEPTION_FILTER'));
+  await app.listen(process.env.NEST_PORT); //TODO 변경
 }
 bootstrap();
