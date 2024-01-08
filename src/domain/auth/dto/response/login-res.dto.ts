5b2ce60b5cc46ab974f16bc9da31fb2344548c8f
HaLin Kim <halin130@tukorea.ac.kr>
2024-01-08 15:26:20 +0900

    feat: login 응답에 사용자 정보 포함 로직 추가

--- src/domain/auth/dto/response/login-res.dto.ts
+++ src/domain/auth/dto/response/login-res.dto.ts
@@ -4,6 +4,19 @@ export class LoginResDto {
   outStayFrDt: string[];
   outStayStGbn: string[];
   outStayToDt: string[];
-  tmGbn: string;
-  yy: string;
+  semester: string;
+  year: string;
+
+  private constructor(cookies: string, name: string, semester: string, year: string) {
+    this.cookies = cookies;
+    this.name = name;
+    this.outStayFrDt = null;
+    this.outStayStGbn = null;
+    this.outStayToDt = null;
+    this.semester = semester;
+    this.year = year;
+  }
+  public static of(cookies: string, name: string, semester: string, year: string): LoginResDto {
+    return new LoginResDto(cookies, name, semester, year);
+  }
 }
